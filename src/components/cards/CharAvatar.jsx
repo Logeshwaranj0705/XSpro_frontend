8fb611dae1e57498edd508abb2d9b2a2682876f5
logesh_07_05 <naru8sasu9344@gmail.com>
2026-01-03 17:47:14 +0530

    Remove unused colors from CharAvatar component
    Removed unused background color options from CharAvatar.

--- src/components/cards/CharAvatar.jsx
+++ src/components/cards/CharAvatar.jsx
@@ -4,15 +4,7 @@ import { getInitials } from '../../utils/helper';
 const CharAvatar = ({ fullname, width, height , style }) => {
     const getRandomBgColor = () => {
   const colors = [
-    "bg-red-300",
-    "bg-green-300",
     "bg-blue-300",
-    "bg-purple-300",
-    "bg-pink-300",
-    "bg-indigo-300",
-    "bg-orange-300",
-    "bg-teal-300",
-    "bg-cyan-300",
   ];
   const randomIndex = Math.floor(Math.random() * colors.length);
   return colors[randomIndex];
