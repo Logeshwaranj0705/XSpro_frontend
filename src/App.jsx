f18df7b8bab92bb5996af16bdd380ecbc8bef7fa
logesh_07_05 <naru8sasu9344@gmail.com>
2026-01-10 00:25:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import{
 } from "react-router-dom";
 
 import Login from "./pages/Auth/Login";
-import SignUp from './pages/Auth/Signup';
+import SignUp from './pages/Auth/SignUp';
 import Home from "./pages/Dashboard/Home";
 
 import UserProvider from "./context/userContext";
